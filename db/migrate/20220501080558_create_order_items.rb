cce683b3c1fa9640a2b8b6482eb1e28627b6418f
satoru0725 <ap03095@icloud.com>
2022-05-01 17:11:49 +0900

    order_itemモデル、マイグレーション作成

---
+++ db/migrate/20220501080558_create_order_items.rb
@@ -0,0 +1,11 @@
+class CreateOrderItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :order_items do |t|
+      t.integer quantity, null: false
+      t.integer price, null: false
+      t.references product, null: false, foreign_key: true
+      t.references reserve, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
