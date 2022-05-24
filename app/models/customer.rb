8d78b5f3ed73bc51caa5fa867f1c32f82295f1f5
satoru0725 <ap03095@icloud.com>
2022-05-24 14:51:11 +0900

    customer新規登録の単体テストコード作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,14 +5,16 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname
+    validates :nickname, uniqueness: { case_sensitive: false }
     validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11 }
     validates :postal_code,format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-) or Eenter a number in half-width' }
     validates :prefecture
     validates :city
-    validates :town
-    
+    validates :town  
   end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }, on: :create
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -23,6 +25,8 @@ class Customer < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
+
+  validates :fax_number, numericality: { only_integer: true }, length: { in: 10..11 }, if: Proc.new { |user| user.fax_number.present?}
   
   has_one :cart, dependent: :destroy
   has_many :addresses, dependent: :destroy
